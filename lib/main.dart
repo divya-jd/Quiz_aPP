8b5ac2ddf77921c0524b137111f8d8bb7bc56b5d
divya-jd <gorla.joise@gmail.com>
2024-11-26 20:46:24 -0500

    modified main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,34 @@
 import 'package:flutter/material.dart';
-
+import 'screens/setup_screen.dart';
 import 'screens/quiz_screen.dart';
-
-
+import 'screens/summary_screen.dart';
 
 void main() {
-
-  runApp(MyApp());
-
+  runApp(QuizApp());
 }
 
-
-
-class MyApp extends StatelessWidget {
-
+class QuizApp extends StatelessWidget {
   @override
-
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
-      title: 'Trivia Quiz App',
-
+      title: 'Customizable Quiz App',
       theme: ThemeData(primarySwatch: Colors.blue),
-
-      home: QuizScreen(),
-
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SetupScreen(),
+        '/quiz': (context) {
+          final args = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+          return QuizScreen(settings: args);
+        },
+        '/summary': (context) {
+          final args = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+          return SummaryScreen(
+            score: args['score'],
+            questions: args['questions'],
+            userAnswers: args['userAnswers'],
+          );
+        },
+      },
     );
-
   }
-
 }
-
